6f5bc3178341669e87c09ebee7146e1b24b59df1
Javi Miguel <jamg44@gmail.com>
2016-04-15 11:16:57 +0200

    fix types in node api promises

--- src/lib/nodeApi.ts
+++ src/lib/nodeApi.ts
@@ -2,19 +2,19 @@
 
 import * as fs from 'fs';
 
-let readFile = (file, encoding?: any) => {
+let readFile: any = (file, encoding?: any) => {
     return new Promise((resolve, reject) => {
         fs.readFile(file, encoding, (e, d) => e ? reject(e) : resolve(d));
     });
 };
 
-let writeFile = (file, data, options?: any) => {
+let writeFile: any = (file, data, options?: any) => {
     return new Promise((resolve, reject) => {
         fs.writeFile(file, data, options, (e) => e ? reject(e) : resolve());
     });
 };
 
-let access = (path, mode?) => {
+let access: any = (path, mode?) => {
     return new Promise((resolve, reject) => {
         fs.access(path, mode, e => e ? reject(e) : resolve());
     });
