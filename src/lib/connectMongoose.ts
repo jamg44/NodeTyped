1ffff325ab2abcfb7d9425d7111f5214be57dac3
Javi Miguel <jamg44@gmail.com>
2016-09-06 14:55:55 +0200

    use node native Promise implementation in mongoose

--- src/lib/connectMongoose.ts
+++ src/lib/connectMongoose.ts
@@ -4,6 +4,8 @@ let mongoose = require('mongoose');
 let localConfig: any = require('../../localConfig');
 let conn = mongoose.connection;
 
+mongoose.Promise = global.Promise;
+
 conn.on('error', function(err) {
     console.error('mongodb connection error:', err);
     process.exit(1);
@@ -13,8 +15,8 @@ conn.once('open', function() {
     console.info('Connected to Mongodb.');
 });
 
-console.log(`Connecting to ${localConfig.MongodbConnection} ...`);
+console.log(`Connecting to ${localConfig.MongodbConnection.uri} ...`);
 
-mongoose.connect(localConfig.MongodbConnection);
+mongoose.connect(localConfig.MongodbConnection.uri, localConfig.MongodbConnection.options);
 
 export { conn };
